74a7256ec79ca855461d1e80657d6456400591c5
Nirnay Kulshreshtha <nirnaykulshreshtha@gmail.com>
2018-04-16 23:48:19 +0530

    ripple icon creation

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import RippleIcon from './RippleIcon';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<RippleIcon />, document.getElementById('root'));
+registerServiceWorker();
